8624417fd67e35dbaa7dfd9e9917ee037130eb6f
arifmalabar <ridhoarif40@gmail.com>
2024-11-18 21:58:45 +0700

    fixing login middleware

--- routes/web.php
+++ routes/web.php
@@ -32,92 +32,94 @@ Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 /*Route::get('/dashboard', function () {
     return view('dashboard/dashboard', ["nama" => "dashboard"]);
 })->name('dashboard');*/
-
-Route::controller(DashboardController::class)->group(function() {
-    Route::get('/dashboard', 'index')->name('dashboard');
-    Route::get('/informasikost', 'informasiKostJson');
+Route::middleware('auth')->group(function() {
+    Route::controller(DashboardController::class)->group(function() {
+        Route::get('/dashboard', 'index')->name('dashboard');
+        Route::get('/informasikost', 'informasiKostJson');
+    });
+    
+    Route::controller(GedungController::class)->group(function () {
+        //Route::get('/setting_gedung', 'index')->name('admin.gedung.index')->middleware('auth');
+        Route::get('/setting_gedung', 'index')->name('admin.gedung.index');
+        Route::get("/setting_gedung/export_excel/{kode_gedung}", "exportExcel");
+        Route::post('/setting_gedung/store', 'store')->name('gedung.store');
+        Route::get('/setting_gedung/{kode_gedung}/edit', 'edit')->name('gedung.edit');
+        Route::put('/setting_gedung/{kode_gedung}/update', 'update')->name('gedung.update');
+        Route::delete('/setting_gedung/{kode_gedung}/delete', 'delete')->name('gedung.delete');
+        Route::get('/sidebar', 'GedungPenghuni')->name('gedung.GedungPenghuni');
+        Route::get('/gedung/aktifkan/{kode_gedung}', 'aktifkanGedung');
+    
+    });
+    
+    Route::controller(RuanganController::class)->group(function () {
+        Route::get('/setting_ruangan', 'index')->name('ruangan.index');
+        Route::get('/setting_ruang/{kode_gedung}', 'getRuanganByKode');
+        Route::post('/setting_ruangan/store', 'store')->name('ruangan.store');
+        Route::get('/setting_ruangan/{kode_kamar}/edit', 'edit')->name('ruangan.edit');
+        Route::put('/setting_ruangan/{kode_kamar}/update', 'update')->name('ruangan.update');
+        Route::delete('/setting_ruangan/{kode_kamar}/delete', 'delete')->name('ruangan.delete');
+        Route::get("/setting_ruangan/aktifkan_ruangan/{kode_kamar}", "aktifkanKamar");
+    });
+    
+    Route::controller(PenghuniController::class)->group(function () {
+        Route::get('/penghuni_ruang', 'index')->name('penghunicontroller.index');
+        Route::get('/penghuni_ruang/status_ruangan/{id}', 'getRuanganKosong')->name('penghuni.getRuanganKosong');
+        Route::get('/penghuni_ruang/tambah_penghuni', 'halamanTambah')->name('penghuni.halamanTambah');
+        Route::get('/penghuni_ruang/detail_penghuni/{id}', 'detailPenghuni')->name('penghuni.detailPenghuni');
+        Route::get('/penghuni_ruang/getDetailPenghuniData/{id}', 'getDetailPenghuniData')->name('penghuni.getDetailPenghuniData');
+        Route::get('/penghuni_ruang/update_penghuni', 'halamanTambah')->name('penghuni.halamanTambah');
+        Route::get("/penghuni_ruang/by_gedung/{kode_gedung}", "getPenghuniByGedung");
+        Route::post('/penghuni_ruang/store', 'store')->name('penghuni.store');
+        Route::get('/penghuni_ruang/{NIK}/edit', 'edit')->name('penghuni.edit');
+        Route::post('/penghuni_ruang/update', 'update')->name('penghuni.update');
+        Route::delete('/penghuni_ruang/{NIK}/delete', 'delete')->name('penghuni.delete');
+    
+    });
+    
+    Route::controller(PembayaranController::class)->group(function() {
+        Route::get('/pembayaran', 'index')->name('pembayaran.index');
+        Route::get('/get_gedung', 'getDataGedung');
+        Route::get('/get_gedung_byid/{id}', 'getGedungById');
+        Route::get('/get_penghuni', 'getDataPenghuni');
+    });
+    
+    Route::controller(TagihanController::class)->group(function(){
+        Route::get("/tagihan", 'index')->name('tagihan.index');
+        Route::get("/data_tagihan", 'getDataTagihan');
+        Route::post('/tambah_tagihan', 'tambahTagihan');
+        Route::post("/sorting_tagihan", "sortTagihan");
+    });
+    
+    Route::controller(Bayar::class)->group(function () {
+        Route::get("/bayar/{id}", 'index')->name('bayar.index');
+        Route::get("/get_bayar/{nik}", 'getDataPembayaran');
+        Route::get('/get_tahun/{nik}', 'getTahunTagihan');
+        Route::get('/get_bulan/{nik}/{tahun}', 'getBulanTagihan');
+        Route::get('/get_pembayaran_tagihan/{kode_bayar}', 'getDataBayarTagihan');
+        Route::put("/bayar_tagihan", "bayarTagihan");
+    });
+    
+    Route::controller(GrafikPenghuni::class)->group(function () {
+        Route::get("/grafik_penghuni", "index")->name("GrafikPenghuni.index");
+        Route::get('/data_grafik_penghuni', "getDataGrafikPenghuni");
+    });
+    Route::controller(GrafikPendapatan::class)->group(function(){
+        Route::get("/laporan_pendapatan", 'index')->name('grafik_pendapatan.index');
+         Route::get('/data_pendapatan', 'getDataPembayaran');
+         Route::get("/data_pendapatan_gedung", "getDataPembayaranGedung");
+         Route::get("/laporan_pendapatan_gedung/{kode_gedung}/{tahun}", "getLaporanGedung")->name('grafik_pendapatan');
+         Route::get("/cetak_laporan_gedung/{kode_gedung}", "cetakLaporanGedung");
+     });
+    
+    
+    // Route untuk mendapatkan penghuni berdasarkan kode gedung
+    // Route::get('/gedung_penghuni/{kode_gedung}', [GedungController::class, 'showPenghuni'])->name('gedung.penghuni');
+    
+    // Route::get('/gedung/{kode_gedung}/penghuni', [PenghuniController::class, 'showPenghuni'])->name('gedung.penghuni');
+    // Route::get('/penghuni/{kode_gedung}', [PenghuniController::class, 'showPenghuni']);
+    Route::get('/gedung/{kode_gedung}/penghuni', [PenghuniController::class, 'showPenghuni'])->name('gedung.penghuni');
+    
 });
-
-Route::controller(GedungController::class)->group(function () {
-    //Route::get('/setting_gedung', 'index')->name('admin.gedung.index')->middleware('auth');
-    Route::get('/setting_gedung', 'index')->name('admin.gedung.index');
-    Route::get("/setting_gedung/export_excel/{kode_gedung}", "exportExcel");
-    Route::post('/setting_gedung/store', 'store')->name('gedung.store');
-    Route::get('/setting_gedung/{kode_gedung}/edit', 'edit')->name('gedung.edit');
-    Route::put('/setting_gedung/{kode_gedung}/update', 'update')->name('gedung.update');
-    Route::delete('/setting_gedung/{kode_gedung}/delete', 'delete')->name('gedung.delete');
-    Route::get('/sidebar', 'GedungPenghuni')->name('gedung.GedungPenghuni');
-    Route::get('/gedung/aktifkan/{kode_gedung}', 'aktifkanGedung');
-
-});
-
-Route::controller(RuanganController::class)->group(function () {
-    Route::get('/setting_ruangan', 'index')->name('ruangan.index');
-    Route::get('/setting_ruang/{kode_gedung}', 'getRuanganByKode');
-    Route::post('/setting_ruangan/store', 'store')->name('ruangan.store');
-    Route::get('/setting_ruangan/{kode_kamar}/edit', 'edit')->name('ruangan.edit');
-    Route::put('/setting_ruangan/{kode_kamar}/update', 'update')->name('ruangan.update');
-    Route::delete('/setting_ruangan/{kode_kamar}/delete', 'delete')->name('ruangan.delete');
-    Route::get("/setting_ruangan/aktifkan_ruangan/{kode_kamar}", "aktifkanKamar");
-});
-
-Route::controller(PenghuniController::class)->group(function () {
-    Route::get('/penghuni_ruang', 'index')->name('penghunicontroller.index');
-    Route::get('/penghuni_ruang/status_ruangan/{id}', 'getRuanganKosong')->name('penghuni.getRuanganKosong');
-    Route::get('/penghuni_ruang/tambah_penghuni', 'halamanTambah')->name('penghuni.halamanTambah');
-    Route::get('/penghuni_ruang/detail_penghuni/{id}', 'detailPenghuni')->name('penghuni.detailPenghuni');
-    Route::get('/penghuni_ruang/getDetailPenghuniData/{id}', 'getDetailPenghuniData')->name('penghuni.getDetailPenghuniData');
-    Route::get('/penghuni_ruang/update_penghuni', 'halamanTambah')->name('penghuni.halamanTambah');
-    Route::get("/penghuni_ruang/by_gedung/{kode_gedung}", "getPenghuniByGedung");
-    Route::post('/penghuni_ruang/store', 'store')->name('penghuni.store');
-    Route::get('/penghuni_ruang/{NIK}/edit', 'edit')->name('penghuni.edit');
-    Route::post('/penghuni_ruang/update', 'update')->name('penghuni.update');
-    Route::delete('/penghuni_ruang/{NIK}/delete', 'delete')->name('penghuni.delete');
-
-});
-
-Route::controller(PembayaranController::class)->group(function() {
-    Route::get('/pembayaran', 'index')->name('pembayaran.index');
-    Route::get('/get_gedung', 'getDataGedung');
-    Route::get('/get_gedung_byid/{id}', 'getGedungById');
-    Route::get('/get_penghuni', 'getDataPenghuni');
-});
-
-Route::controller(TagihanController::class)->group(function(){
-    Route::get("/tagihan", 'index')->name('tagihan.index');
-    Route::get("/data_tagihan", 'getDataTagihan');
-    Route::post('/tambah_tagihan', 'tambahTagihan');
-    Route::post("/sorting_tagihan", "sortTagihan");
-});
-
-Route::controller(Bayar::class)->group(function () {
-    Route::get("/bayar/{id}", 'index')->name('bayar.index');
-    Route::get("/get_bayar/{nik}", 'getDataPembayaran');
-    Route::get('/get_tahun/{nik}', 'getTahunTagihan');
-    Route::get('/get_bulan/{nik}/{tahun}', 'getBulanTagihan');
-    Route::get('/get_pembayaran_tagihan/{kode_bayar}', 'getDataBayarTagihan');
-    Route::put("/bayar_tagihan", "bayarTagihan");
-});
-
-Route::controller(GrafikPenghuni::class)->group(function () {
-    Route::get("/grafik_penghuni", "index")->name("GrafikPenghuni.index");
-    Route::get('/data_grafik_penghuni', "getDataGrafikPenghuni");
-});
-Route::controller(GrafikPendapatan::class)->group(function(){
-    Route::get("/laporan_pendapatan", 'index')->name('grafik_pendapatan.index');
-     Route::get('/data_pendapatan', 'getDataPembayaran');
-     Route::get("/data_pendapatan_gedung", "getDataPembayaranGedung");
-     Route::get("/laporan_pendapatan_gedung/{kode_gedung}/{tahun}", "getLaporanGedung")->name('grafik_pendapatan');
-     Route::get("/cetak_laporan_gedung/{kode_gedung}", "cetakLaporanGedung");
- });
-
-
-// Route untuk mendapatkan penghuni berdasarkan kode gedung
-// Route::get('/gedung_penghuni/{kode_gedung}', [GedungController::class, 'showPenghuni'])->name('gedung.penghuni');
-
-// Route::get('/gedung/{kode_gedung}/penghuni', [PenghuniController::class, 'showPenghuni'])->name('gedung.penghuni');
-// Route::get('/penghuni/{kode_gedung}', [PenghuniController::class, 'showPenghuni']);
-Route::get('/gedung/{kode_gedung}/penghuni', [PenghuniController::class, 'showPenghuni'])->name('gedung.penghuni');
 
 
 // Route::get("/login", function() {
